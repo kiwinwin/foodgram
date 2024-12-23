8cbd5d1650161a0af0cb0651f16c010257173bac
kiwinwin <galia.baibulova98@gmail.com>
2024-12-23 04:55:12 +0300

    edited_js

--- frontend/src/pages/about/index.js
+++ frontend/src/pages/about/index.js
@@ -38,10 +38,10 @@ const About = ({ updateOrders, orders }) => {
           </h2>
           <div className={styles.text}>
             <p className={styles.textItem}>
-              Код проекта находится тут - <a href="https://github.com/kiwinwin/foodgram" className={styles.textLink}>Github</a>
+              Код проекта находится тут - <a href="https://github.com/kiwinwin/foodgram" target="_blank">Github</a>
             </p>
             <p className={styles.textItem}>
-              Автор проекта: <a href="https://github.com/kiwinwin/" className={styles.textLink}>Имя Автора</a>
+              Автор проекта: <a href="https://github.com/kiwinwin/" target="_blank">kiwinwin</a>
             </p>
           </div>
         </aside>
